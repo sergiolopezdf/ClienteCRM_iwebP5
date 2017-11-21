27f41ccc0bb7b2ddfcd3ea8b663da632d3f0da08
Sergio López <sergio.lopezd@alumnos.upm.es>
2017-11-21 18:41:51 +0100

    Initial Commit

---
+++ CRMClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CRMClient
+//
+//  Created by Sergio López on 21/11/2017.
+//  Copyright © 2017 UPM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
