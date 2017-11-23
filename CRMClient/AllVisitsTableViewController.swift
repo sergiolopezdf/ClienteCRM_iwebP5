3590b4701fad474d601dc9d2291f5dbd87a95b7c
Sergio López <sergio.lopezd@alumnos.upm.es>
2017-11-23 19:55:22 +0100

    JSON download + Img download is asynchronus now

--- CRMClient/AllVisitsTableViewController.swift
+++ CRMClient/AllVisitsTableViewController.swift
@@ -14,115 +14,139 @@ class AllVisitsTableViewController: UITableViewController {
     let targetURL = "https://dcrmt.herokuapp.com/api/visits/flattened?token="
     var stringURL = ""
     var visits: [Visits] = []
+    var salesmanImg = [Int:UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
         stringURL = "\(targetURL)\(token)"
-        guard let salesmen = getDataFromJSON() else {
-            print("No data from JSON")
-            return
-        }
+        getDataFromJSON()
+        tableView.reloadData()
         
-        self.visits = salesmen
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return visits.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VisitsCell", for: indexPath)
         
+        //Actualizamos el texto de las celdas
         cell.textLabel?.text = visits[indexPath.row].Customer.name
         cell.detailTextLabel?.text = visits[indexPath.row].Salesman.fullname
-        cell.imageView?.image = UIImage(named: "head-512")
+        
+        //Id del vendedor
+        let salesmanId = visits[indexPath.row].Salesman.id
+        
+        //Si no hay URL de la foto, asignamos foto por defecto en el array
+        guard let salesmanUrlStg = visits[indexPath.row].Salesman.Photo?.url else {
+            cell.imageView?.image = UIImage(named: "head-512")
+            return cell
+        }
+        
+        //Si no existe la foto, te la descargas
+        guard let img = salesmanImg[salesmanId]  else {
+            getSalesmanImg(id: salesmanId, URLString: salesmanUrlStg, for: indexPath)
+            return cell
+        }
+        
+        //Siempre se cumple porque en la función getSalesmanImg haces reload de la celda
+        
+        //Asignas la foto del array a la celda
+        cell.imageView?.image = img
+        
         
         return cell
     }
- 
-    private func getDataFromJSON() -> [Visits]? {
-        guard let url = URL(string: stringURL)  else {
-            print("Error en la URL")
-            return nil
-        }
+    
+    //Usamos GCD para emplear otro Thread y Data para descargar el JSON
+    private func getDataFromJSON() {
         
+        //Creamos cola global
+        let queue = DispatchQueue.global()
+        
+        //Activamos el indicador de red
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         defer {
-             UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
         }
         
-        guard let data = try? Data(contentsOf: url) else {
-            print("Error en la descarga del JSON")
-            return nil
+        queue.async {
+            //Creamos la URL
+            guard let url = URL(string: self.stringURL) else {
+                print("Error en la URL")
+                return
+            }
+            
+            //Usamos Data para la descarga
+            guard let data = try? Data(contentsOf: url) else {
+                print("Error en la descarga del JSON")
+                return
+            }
+            
+            //Creamos el decodificador de JSON
+            let decoder = JSONDecoder()
+            
+            //Decodificamos de JSON
+            guard let decodedVisits = try? decoder.decode([Visits].self, from: data) else {
+                print("JSON no decodificado")
+                return
+            }
+            
+            print("JSON Ok")
+            //print(decodedVisits)
+            
+            //Añadimos al array
+            DispatchQueue.main.async {
+                print("Vuelta main")
+                self.visits = decodedVisits
+                self.tableView.reloadData()
+            }
+            
         }
-
-        let decoder = JSONDecoder()
+        
+    }
     
-        guard let decodedSalesman = try? decoder.decode([Visits].self, from: data) else {
-            print("JSON no decodificado")
-            return nil
+    private func getSalesmanImg(id: Int, URLString stgUrl: String, for indexPath: IndexPath) {
+        let queue = DispatchQueue.global()
+        
+        queue.async {
+            //Creamos la URL
+            guard let url = URL(string: stgUrl) else {
+                print("Error en la URL")
+                return
+            }
+            
+            //Usamos Data para la descarga
+            guard let data = try? Data(contentsOf: url) else {
+                print("Error en la descarga de la imagen")
+                return
+            }
+            
+            //Añadimos la imagen al diccionario
+            if let img = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.salesmanImg[id] = img
+                    self.tableView.reloadRows(at: [indexPath], with: .fade)
+                }
+            }
+            
         }
         
-        return decodedSalesman
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
